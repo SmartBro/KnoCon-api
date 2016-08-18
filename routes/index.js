253e2579dd19e104a355a354cd0f1d11517cd26f
SmartBro <smartbrostudio@gmail.com>
2016-08-18 23:44:13 +0300

    allow CORS for localhost:8080

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const apiRouter = require('express').Router();
 const booksRouter = require('./books');
 
+apiRouter.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 apiRouter.use('/books', booksRouter);
 
 module.exports = apiRouter;
